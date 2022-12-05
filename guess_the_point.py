ff491cc060835071c86b03d594f9a1473495ea1c
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2022-12-05 09:42:39 +0100

    Initial commit with adding guess_the_point.py file

---
+++ guess_the_point.py
@@ -0,0 +1,57 @@
+from random import randint
+
+
+def game():
+    """The game of searching point.
+
+    Rules:
+    1. At first the program shows coordinates of the rectangle to the user (they are randomly selected),
+    then the user has to guess the point from inside the rectangle by enter the coordinates of the point.
+    2. The coordinates of the points can be randomized (and guessing) from the range 0-100.
+    """
+
+    random_rectangle = Rectangle()
+    print("Welcome to the game! Your task is to choose the point that falls in the rectangle.")
+    print(f"The coordinates of our rectangle are: {random_rectangle.show_both_points()}\n")
+    coordinate_x = int(input("Type first coordinate:\n>>> "))
+    coordinate_y = int(input("Type second coordinate:\n>>> "))
+    user_game = Game((coordinate_x, coordinate_y))
+    print(user_game.check_the_point(random_rectangle.show_both_points()))
+
+
+class Rectangle:
+    # class for choose two random points of the rectangle
+
+    def __init__(self):
+        self.point_1 = randint(0, 100), randint(0, 100)
+        self.point_2 = randint(0, 100), randint(0, 100)
+
+    def validation_points(self):
+        while self.point_2 == self.point_1:
+            self.point_2 = randint(0, 100)
+        return self.point_2
+
+    def show_both_points(self):
+        return self.point_1, self.validation_points()
+
+
+class Game:
+    # check if the user's point falls in the selected rectangle, return True or False
+
+    def __init__(self, user_point):  # user_point -> tuple
+        self.user_point = user_point
+
+    def check_the_point(self, points):
+        point1 = points[0]
+        point2 = points[1]
+        scope_x = min(point1[0], point2[0]), max(point1[0], point2[0])
+        scope_y = min(point1[1], point2[1]), max(point1[1], point2[1])
+
+        if scope_x[0] <= self.user_point[0] <= scope_x[1] and scope_y[0] <= self.user_point[1] <= scope_y[1]:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    game()
